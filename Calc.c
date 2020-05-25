333cafc0ea2bb28ccfbe5d2553ada7af987343a0
Junduck15 <63302994+Junduck15@users.noreply.github.com>
2020-05-25 23:48:02 +0900

    Update and rename Calc.c to Calc.cpp

--- Calc.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-  printf("Please write down the equation");
-  return 0;
-}
---
+++ Calc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a,b;
+  char c;
+  cout << "Please write down the equation";
+  cin >> a >> c >> b;
+  return 0;
+}
